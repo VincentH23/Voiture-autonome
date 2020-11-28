a9f03cb5b970e44fab268952fa341669d6f2bdc7
lucaschardin <lucas.chardin@hotmail.fr>
2020-11-28 17:09:59 +0100

    envi avec les règles

--- environment.py
+++ environment.py
@@ -3,7 +3,7 @@ import road
 import Voiture
 
 
-class environment:
+class Environment:
 
     def __init__(self,size_road,  number_of_turn, x, y, n, p, init_pos = (0,0),
                  size_matrix = (360,360)):
@@ -11,4 +11,37 @@ class environment:
                  size_matrix = (360,360))
         self.car = Voiture.Voiture(x, y, n, p,size_matrix = (360,360))
         self.envi = self.my_road.map + self.car.map
+        self.score = 0
+        self.game_over = False
+        self.data = [self.score, self.envi, self.game_over]
+
+    def change_envi(self,y,x,new_value):
+        self.envi[y][x] = new_value
+
+
+    def change_game_over(self,y,x):
+        if self.envi[y][x] == 5:
+            self.game_over = True
+
+
+    def rules(self):
+        path_ = self.my_road.path
+        for road_segment in path_:
+            if road_segment[2] in [0,1]:
+                self.change_game_over(road_segment[0] + (self.my_road._size_road//2) - 1,road_segment[1])
+                self.change_game_over(road_segment[0] - (self.my_road._size_road // 2) + 1, road_segment[1])
+            else :
+                self.change_game_over(road_segment[0], road_segment[1] + (self.my_road._size_road//2) - 1)
+                self.change_game_over(road_segment[0], road_segment[1] - (self.my_road._size_road // 2) + 1)
+            if self.game_over == True:
+                break
+
+
+
+
+
+
+
+
+
 

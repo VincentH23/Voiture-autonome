f1a05fd4eb3a5380f52cd6d95b385d2e9f9262cd
Vincent <vincenthuang148@gmail.com>
2020-12-04 17:35:06 +0100

    ajout de Next stape dans l'environement manque encore le fonction pour actualiser le score

--- environment.py
+++ environment.py
@@ -10,31 +10,35 @@ class Environment:
         self.my_road = road.Road(size_road,  number_of_turn, init_pos = (0,0),
                  size_matrix = (360,360))
         self.car = Voiture.Voiture(x, y, n, p,size_matrix = (360,360))
-        self.envi = self.my_road.map + self.car.map
+        self.window = self.my_road.map + self.car.map
         self.score = 0
         self.game_over = False
-        self.data = [self.score, self.envi, self.game_over]
-
-    def change_envi(self,y,x,new_value):
-        self.envi[y][x] = new_value
-
-
-    def change_game_over(self,y,x):
-        if self.envi[y][x] == 5:
-            self.game_over = True
+        self.data = [self.score, self.window, self.game_over]
+        self.DeltaAngle=0.05
+        self.vitesse=5
+        self.acc=1
+        self.decelaration=-1
+        self.vitesseMin=5
 
 
-    def rules(self):
-        path_ = self.my_road.path
-        for road_segment in path_:
-            if road_segment[2] in [0,1]:
-                self.change_game_over(road_segment[0] + (self.my_road._size_road//2) - 1,road_segment[1])
-                self.change_game_over(road_segment[0] - (self.my_road._size_road // 2) + 1, road_segment[1])
-            else :
-                self.change_game_over(road_segment[0], road_segment[1] + (self.my_road._size_road//2) - 1)
-                self.change_game_over(road_segment[0], road_segment[1] - (self.my_road._size_road // 2) + 1)
-            if self.game_over == True:
-                break
+    def get_data(self):
+        return self.data
+
+    def nextStep(self):
+
+        if self.data['gameover'] == False:
+            self.data["step"] += 1
+            decision =self.car.decision(self.get_data())
+            self.vitesse+=decision[0]*self.acc+(1-decision[0])*self.decelaration
+            self.vitesse=max(self.vitesse,self.vitesseMin)
+            angle=(decision[1]-decision[2])*self.DeltaAngle
+            self.car.deplacement(self.vitesse,angle)
+            #Fonction pour a ctualiser le score à faire
+            self.window=self.my_road.map+self.car.map
+            A=self.window[self.window>5]
+            if A.shape[0]==0:
+                self.game_over=True
+
 
 
 

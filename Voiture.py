5c3cf1a24772ac5c7d94f734c80e5295eea6a10d
vincentH <vincent.huang@centrale-marseille.fr>
2020-12-11 17:33:34 +0100

    ajout du cerveau

--- Voiture.py
+++ Voiture.py
@@ -1,6 +1,28 @@
 import numpy as np
 
 import matplotlib.pyplot as plt
+import tensorflow as tf
+class Brain:
+    def __init__(self,input_shape):
+        self.input_shape=input_shape
+        self.layer1=tf.keras.layers.Conv2D(16,kernel_size=(5,5),activation='relu',input_shape=input_shape)
+        self.layer2=tf.keras.layers.Conv2D(32,kernel_size=(3,3),strides=(2,2),activation='tanh')
+        self.layer3=tf.keras.layers.Dense(32,activation='tanh')
+        self.layer4=tf.keras.layers.Dense(3,activation='sigmoid')
+
+    def forward(self,x,batch_size=1):
+        new_shape=(batch_size,)+self.input_shape     #add the batch size and the channel
+        out=x.reshape(new_shape)
+        out=self.layer1(out)
+        out=self.layer2(out)
+        out=tf.keras.layers.Flatten()(out)
+        out=self.layer3(out)
+        out=self.layer4(out)
+        return out
+
+    def decision(self,x):
+        pred=self.forward(x)
+        return pred[0]>np.array([0.5,0.5,0.5])
 
 class Voiture:
 
@@ -8,6 +30,7 @@ class Voiture:
         self.pos=np.array([[x],[y]]).astype('float64')
         self.direction=np.array([[-1],[0]]).astype('float64')
         self.size=n,p
+        self.brain=Brain(size_matrix+(1,))                      # add the channel
         self.map=np.zeros(size_matrix)
 
         self.map[y-p//2:y+p//2+1,x-n//2:x+n//2+1]=1
